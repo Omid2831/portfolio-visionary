8334bb368aa428f522553215d0c00e82208dbbe0
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-24 15:14:43 +0100

    feat: restructure PortfolioSection with improved headings and add portfolio-section styles

--- src/components/LandingSection/PortfolioSection.jsx
+++ src/components/LandingSection/PortfolioSection.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import '../../styles/portfolio-section.css'
+
 
 const PortfolioSection = ({ children }) => {
   return (
     <section className="portfolio-section max-w-screen flex flex-col items-center gap-4 px-2 py-4">
-      <p className='text-white'>Hello I'm Omid Mehrabi</p>
-      <h2 className='text-white'>Software Developer</h2>
+      <div>
+        <p className='head-line text-center text-white'>Hello, I'm Omid Mehrabi</p>
+        <h2 className='sup-head-line text-center text-white'>Frontend Engineer</h2>
+      </div>
       <div className='flex flex-col'>
         {children}
       </div>
