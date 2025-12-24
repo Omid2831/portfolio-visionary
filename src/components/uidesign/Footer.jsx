1dfb09101c0c06e02e1331520145d9cec3f4454d
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-25 00:09:14 +0100

    feat: update Footer component to display the current year in copyright notice

--- src/components/uidesign/Footer.jsx
+++ src/components/uidesign/Footer.jsx
@@ -1,7 +1,10 @@
 const Footer = () => {
+
+  // Get the current year for the copyright notice
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="flex items-center justify-center w-full text-gray-400 py-4 border-t border-gray-700 mt-8">
-      <p className="text-sm">&copy; 2025 My Application, My Portfolio</p>
+      <p className="text-sm">&copy; {currentYear} My Application, My Portfolio</p>
     </footer>
   )
 }
