c3126b7a8b2f6176968855a8ab8d82bf11f325ca
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-24 12:35:18 +0100

    fix: update social media links in SocialLinksData with correct URLs

--- src/shared/data/social-links.js
+++ src/shared/data/social-links.js
@@ -1,17 +1,17 @@
 export const SocialLinksData = [
     {
         name: "Github",
-        url: "https://github.com/yourusername",
+        url: "https://github.com/omid2831",
         icon: "Github",
     },
     {
         name: "Linkedin",
-        url: "https://linkedin.com/in/yourusername",
+        url: "https://linkedin.com/in/omid-mehrabi-2638792a5/",
         icon: "Linkedin",
     },
     {
         name: "Mail",
-        url: "mailto:your@email.com",
+        url: "mehrabiomid8282@gmail.com",
         icon: "Mail",
     },
 ];
