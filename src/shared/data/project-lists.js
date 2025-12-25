5c2987d95b161723ae4f4df7ed54e2cfdd223e59
Omid2831 <mehrabiomid8282@gmail.com>
2025-12-25 21:16:05 +0100

    feat: add project lists data for portfolio showcasing

---
+++ src/shared/data/project-lists.js
@@ -0,0 +1,30 @@
+export const projectLists = [
+    {
+        id: 1,
+        title: "Pokemon Website",
+        image: "pikachu.png",
+        description: "Pokémon Manager is a React-based web application designed to manage and explore Pokémon data dynamically using the PokéAPI",
+        technologies: ["React", "Tailwind CSS", "TypeScript", "Zustand"],
+        link: "https://pokemon-manager-one.vercel.app/",
+        github: "https://github.com/Omid2831/PokemonManager"
+    },
+    {
+        id: 2,
+        title: "Authentication - Role Based Website",
+        image: "authentication.png",
+        description: "his project is a role-based authentication system built with Laravel and Laravel Breeze react Based",
+        technologies: ["React", "Tailwind CSS", "Javascript", "Laravel", "Laravel Breeze", "MySQL", "Database Design"],
+        link: "",
+        github: "https://github.com/Omid2831/authentication-app"
+    },
+    {
+        id: 3,
+        title: "Sneakerness - E-commerce Website",
+        image: "sneakerness.png",
+        description: "Sneakerness is a Laravel-based e-commerce platform specializing in sneakers, offering features like product browsing, shopping cart, and secure checkout.",
+        technologies: [ "Notification system","Email Verification system"," ai chat bot", "Javascript", "Laravel", "MySQL", "Database Design"],
+        link: "",
+        github: "https://github.com/Omid2831/Sneakerness"
+    },
+
+];
