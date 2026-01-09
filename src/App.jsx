48fe82df8a505fb7c03d3422a9ed56867935290a
Omid2831 <mehrabiomid8282@gmail.com>
2026-01-09 18:56:20 +0100

    feat: import and render FormSection component in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import PortfolioSection from "./components/LandingSection/PortfolioSection"
 import VideoIntroPortfolio from "./components/LandingSection/VideoIntroPortfolio"
 import QuestionedAskedAccordion from "./components/uidesign/QuestionedAskedAccordian"
 import ProjectList from "./components/ProjectSection/ProjectList"
+import FormSection from "./components/FormSender/FormSection"
+
 
 const App = () => {
   return (
@@ -22,10 +24,11 @@ const App = () => {
 
       {/* Project Cards Section */}
       <div>
-          {/** Project Title */}
+        {/** Project Title */}
         <h2 className="mb-8 text-5xl font-bold text-white text-center">Projects</h2>
         <ProjectList />
       </div>
+      <FormSection />
     </RootLayout>
   )
 }
