f208168aee1116c4499a520321059592d18668a2
Omid2831 <mehrabiomid8282@gmail.com>
2026-01-09 20:31:09 +0100

    feat: refactor homepage by removing unused components and adding Hero, HeroContent, and HeroImage components

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,7 @@ import FormSection from "./components/FormSender/FormSection"
 const App = () => {
   return (
     <RootLayout>
-      <PortfolioSection>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8 lg:gap-12 max-w-7xl mx-auto items-start">
-          <div className="landing-profile-section flex flex-col items-center w-full">
-            <CardPortfolio />
-          </div>
-          <div className="landing-video-section w-full h-full">
-            <VideoIntroPortfolio />
-          </div>
-        </div>
-      </PortfolioSection>
-      <QuestionedAskedAccordion />
-
-      {/* Project Cards Section */}
-      <div>
-        {/** Project Title */}
-        <h2 className="mb-8 text-5xl font-bold text-white text-center">Projects</h2>
-        <ProjectList />
-      </div>
-      <FormSection />
+      
     </RootLayout>
   )
 }
